556166e684b7ef7c1f84a09852bcb03063e738bd
rashmipbhat <rashmipbhat97@gmail.com>
2024-02-21 12:52:58 -0800

    Added context and api folder linking flickr api

---
+++ my-app/src/context/PhotoContext.js
@@ -0,0 +1,27 @@
+import React, {createContext, useState} from "react";
+import axios from "axios";
+export const PhotoContext =createContext();
+
+
+const PhotoContextProvider = props =>{
+const [images,setImages]=useState([]);
+const [loading,setLoading]=useState(true);
+
+const runsearch=()=>{
+    axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then(response =>{
+        setImages(response.data);
+        setLoading(false);
+    })
+    .catch(error =>{
+        console.log("Encountered an error with fetching and p[arsing data",error);
+    })
+};
+return(
+    <PhotoContext.Provider value={{images,loading,runsearch}}>
+        {props.children}
+    </PhotoContext.Provider>
+);
+}
+export default PhotoContextProvider;
+
